254f4e27b9660ba37c60c3fc7956df24d52656f2
PatrickDominick <pdneary@gmail.com>
2021-09-02 13:02:52 -0500

    site lined up with placeholders

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route path="/signup" component={() => <div>Signup</div>} />
+          <Route path="/rules" component={() => <div>Rules</div>} />
+          <Route path="/game" component={() => <div>Game</div>} />
+          <Route path="/gameover" component={() => <div>Gameover</div>} />
+
+        </Switch>
       </div>
     );
   }
